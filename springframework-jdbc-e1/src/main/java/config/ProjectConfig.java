80d5d0fc408f941a15ba6962070844a6d658b2e9
Mozif Beigh <mmozif03@gmail.com>
2022-02-01 13:47:56 +0530

    commit message

---
+++ springframework-jdbc-e1/src/main/java/config/ProjectConfig.java
@@ -0,0 +1,31 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = "repositories")
+public class ProjectConfig {
+
+    @Bean
+    public DataSource dataSource(){
+        var dataSource =  new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost/demo");
+        dataSource.setUsername("root");
+        dataSource.setPassword("password");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+
+
+}
